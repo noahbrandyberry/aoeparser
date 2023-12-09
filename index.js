874a5c7b0a6b6aaf639276d795d3713928a4d0a0
Noah Brandyberry <noahbrandyberry@gmail.com>
2023-12-09 12:07:05 -0500

    chore: ran prettier

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const fs = require('fs');
-const FireStoreParser = require('firestore-parser');
-const lodash = require('lodash');
-const repair = require('jsonrepair')
+const fs = require("fs");
+const FireStoreParser = require("firestore-parser");
+const lodash = require("lodash");
+const repair = require("jsonrepair");
 
-const input = fs.readFileSync('input', 'utf-8')
-const response = JSON.parse(repair.jsonrepair(input))
+const input = fs.readFileSync("input", "utf-8");
+const response = JSON.parse(repair.jsonrepair(input));
 
-let formatted = FireStoreParser(response)
-formatted = formatted.filter((doc) => typeof doc === "object")
-formatted = lodash.flatten(formatted)
-formatted = lodash.flatten(formatted)
-formatted = formatted.filter((doc) => typeof doc === "object")
-formatted = lodash.flatten(formatted)
-formatted = formatted.filter((doc) => doc.documentChange)
-formatted = formatted.map((doc) => doc.documentChange.document.fields)
+let formatted = FireStoreParser(response);
+formatted = formatted.filter((doc) => typeof doc === "object");
+formatted = lodash.flatten(formatted);
+formatted = lodash.flatten(formatted);
+formatted = formatted.filter((doc) => typeof doc === "object");
+formatted = lodash.flatten(formatted);
+formatted = formatted.filter((doc) => doc.documentChange);
+formatted = formatted.map((doc) => doc.documentChange.document.fields);
 
-console.log(formatted)
-
-if (!fs.existsSync('outputs')) {
-  fs.mkdirSync('outputs');
+if (!fs.existsSync("outputs")) {
+  fs.mkdirSync("outputs");
 }
 
-fs.writeFile(`outputs/builds-${(new Date()).getTime()}.json`, JSON.stringify(formatted, null, 2), function(err) {
-  if (err) {
+fs.writeFile(
+  `outputs/builds-${new Date().getTime()}.json`,
+  JSON.stringify(formatted, null, 2),
+  function (err) {
+    if (err) {
       console.log(err);
+    }
   }
-});
+);
+
+console.log(`Saved file to outputs/builds-${new Date().getTime()}.json`);
